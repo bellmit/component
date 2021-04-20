1aef5d7800235518b0880451fa4ce538068c4c81
lilou <lilou@coocaa.com>
2021-04-20 10:04:21 +0800

    add: component-domain-starter

---
+++ component-domain-starter/Readme.md
@@ -0,0 +1,2 @@
+参考了cola
+中的组件： [cola-component-domain-starter](https://github.com/alibaba/COLA/tree/master/cola-components/cola-component-domain-starter)
---
+++ component-domain-starter/src/main/java/com/lyloou/component/domain/ApplicationContextHelper.java
@@ -0,0 +1,64 @@
+package com.lyloou.component.domain;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * ApplicationContextHelper
+ *
+ * @author Frank Zhang
+ * @date 2020-11-14 1:58 PM
+ */
+@Component
+public class ApplicationContextHelper implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextHelper.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> targetClz) {
+        T beanInstance = null;
+        //优先按type查
+        try {
+            beanInstance = applicationContext.getBean(targetClz);
+        } catch (Exception e) {
+            // ignore
+        }
+        //按name查
+        if (beanInstance == null) {
+            String simpleName = targetClz.getSimpleName();
+            //首字母小写
+            simpleName = Character.toLowerCase(simpleName.charAt(0)) + simpleName.substring(1);
+            //noinspection unchecked
+            beanInstance = (T) applicationContext.getBean(simpleName);
+        }
+
+        //noinspection ConstantConditions
+        if (beanInstance == null) {
+            throw new RuntimeException("Component " + targetClz + " can not be found in Spring Container");
+        }
+        return beanInstance;
+    }
+
+    public static Object getBean(String clazz) {
+        return ApplicationContextHelper.applicationContext.getBean(clazz);
+    }
+
+    public static <T> T getBean(String name, Class<T> requiredType) {
+        return ApplicationContextHelper.applicationContext.getBean(name, requiredType);
+    }
+
+    @SuppressWarnings("AlibabaVarargsParameter")
+    public static <T> T getBean(Class<T> requiredType, Object... params) {
+        return ApplicationContextHelper.applicationContext.getBean(requiredType, params);
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+}
