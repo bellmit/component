ba043592c72f358789a7f8693972b151312a23ad
lilou <lilou@coocaa.com>
2021-07-15 19:17:44 +0800

    component-logger-request-statistic-starter: update

--- component-logger-request-statistic-starter/src/main/java/com/lyloou/component/loggerrequeststatistic/RequestWrapper.java
+++ component-logger-request-statistic-starter/src/main/java/com/lyloou/component/loggerrequeststatistic/RequestWrapper.java
@@ -1,61 +1,40 @@
 package com.lyloou.component.loggerrequeststatistic;
 
+import cn.hutool.core.io.IoUtil;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * [springboot 解决拦截器读取post请求输入流参数body内容后controller中 @RequestBody 参数无法读取body 内容问题,源码demo_a704397849的博客-CSDN博客](https://blog.csdn.net/a704397849/article/details/97267572)
  *
  * @author lilou
  */
+@Slf4j
 public class RequestWrapper extends HttpServletRequestWrapper {
-    private final String body;
+    private String body;
 
     public RequestWrapper(HttpServletRequest request) {
         super(request);
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = null;
-        InputStream inputStream = null;
         try {
-            inputStream = request.getInputStream();
-            if (inputStream != null) {
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                char[] charBuffer = new char[128];
-                int bytesRead = -1;
-                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-                    stringBuilder.append(charBuffer, 0, bytesRead);
-                }
-            } else {
-                stringBuilder.append("");
-            }
+            body = IoUtil.readUtf8(request.getInputStream());
         } catch (IOException ex) {
-
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            body = "";
+            log.warn("读取数据流失败", ex);
         }
-        body = stringBuilder.toString();
     }
 
     @Override
-    public ServletInputStream getInputStream() throws IOException {
+    public ServletInputStream getInputStream() {
         final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
-        ServletInputStream servletInputStream = new ServletInputStream() {
+        return new ServletInputStream() {
             @Override
             public boolean isFinished() {
                 return false;
@@ -71,16 +50,15 @@ public class RequestWrapper extends HttpServletRequestWrapper {
             }
 
             @Override
-            public int read() throws IOException {
+            public int read() {
                 return byteArrayInputStream.read();
             }
         };
-        return servletInputStream;
 
     }
 
     @Override
-    public BufferedReader getReader() throws IOException {
+    public BufferedReader getReader() {
         return new BufferedReader(new InputStreamReader(this.getInputStream()));
     }
 
