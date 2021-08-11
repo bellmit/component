2533d9d5cd3e849ea7adbc227955e2948cd6e8f3
lilou <lilou@coocaa.com>
2021-08-11 11:36:15 +0800

    component-key-value-item-app: update

--- component-key-value-item-app/src/main/java/com/lyloou/component/keyvalueitem/executor/query/KeyValueItemPageQryExe.java
+++ component-key-value-item-app/src/main/java/com/lyloou/component/keyvalueitem/executor/query/KeyValueItemPageQryExe.java
@@ -3,6 +3,7 @@ package com.lyloou.component.keyvalueitem.executor.query;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.lyloou.component.dto.ConvertUtils;
 import com.lyloou.component.dto.PageInfoHelper;
 import com.lyloou.component.keyvalueitem.dto.CacheNames;
 import com.lyloou.component.keyvalueitem.dto.clientobject.KeyValueItemCo;
@@ -15,6 +16,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -61,37 +63,35 @@ public class KeyValueItemPageQryExe {
      */
     @Cacheable(value = CacheNames.KeyValueItemCo_PAGE_KEY, key = "#qry.itemName + '::' + #qry.itemKey+'::' + #qry.cachePageKey")
     public PageInfo<KeyValueItemCo> execute(KeyValueItemPageQry qry) {
-        final Page<Object> page = PageHelper.startPage(qry.getPageNum(), qry.getPageSize());
+
+        final Page<Object> page = PageHelper.startPage(qry.getPageNum(), qry.getPageSize(), qry.isNeedTotalCount(), false, false);
         if (!Strings.isEmpty(qry.getOrderBy())) {
             page.setOrderBy(qry.getOrderBy().concat(" ").concat(qry.getOrderDirection()));
         }
-        page.setCount(qry.isNeedTotalCount());
 
         // 先查id
-        final List<Integer> idList = keyValueItemService.lambdaQuery()
+        final List<KeyValueItemEntity> itemEntities = keyValueItemService.lambdaQuery()
                 .select(KeyValueItemEntity::getId)
                 .eq(qry.getItemName() != null, KeyValueItemEntity::getItemName, qry.getItemName())
                 .eq(qry.getItemKey() != null, KeyValueItemEntity::getItemKey, qry.getItemKey())
                 .eq(KeyValueItemEntity::getDeleted, 0)
-                .list()
+                .list();
+        final List<Integer> idList = itemEntities
                 .stream().map(KeyValueItemEntity::getId)
                 .collect(Collectors.toList());
 
+        if (CollectionUtils.isEmpty(idList)) {
+            return new PageInfo<>();
+        }
         // 再根据id查数据
-        final List<KeyValueItemEntity> entityList = keyValueItemService.lambdaQuery()
+        final List<KeyValueItemCo> list = keyValueItemService.lambdaQuery()
                 .in(KeyValueItemEntity::getId, idList)
-                .list();
-
-        // 转化
-        final List<KeyValueItemCo> list = entityList
+                .list()
+                // 转化
                 .stream()
-                .map(keyValueItemEntity -> {
-                    final KeyValueItemCo keyValueItemCo = new KeyValueItemCo();
-                    BeanUtils.copyProperties(keyValueItemEntity, keyValueItemCo);
-                    return keyValueItemCo;
-                })
+                .map(keyValueItemEntity -> ConvertUtils.convert(keyValueItemEntity, KeyValueItemCo.class))
                 .collect(Collectors.toList());
-        return PageInfoHelper.getPageInfo(idList, list);
+        return PageInfoHelper.getPageInfo(itemEntities, list);
 
     }
 }
