5cf9d71b2ed7d332be3f383b464fbede3eeaa105
lilou <lilou@coocaa.com>
2022-01-13 14:36:05 +0800

    upgrade 1.0.16

--- bin/deploy.cmd
+++ bin/deploy.cmd
@@ -1,2 +1,6 @@
-# 使用 cmd 运行，因为在sh模式下，gpg --list-key 没有值
+@REM 使用 cmd 运行，因为在sh模式下，gpg --list-key 没有值
+
+@REM 手动逐行运行
+start cmd
+gpg --list-key
 mvn -s C:/Users/lilou/.m2/settings_ossrh.xml clean deploy -P release
